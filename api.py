58c1619de8789a438d21c62ae1dee02150d02d35
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-04-07 16:08:54 +0300

    Added predicting functionality to API
    Model is now loaded and used to predict the price of bitcoin from the given date(time)

--- api.py
+++ api.py
@@ -1,13 +1,32 @@
+import pickle
+import dateutil.parser as datetimeParser
+import pandas as pd
 from flask import Flask, jsonify, request, Response
+
+# Open the file in binary mode
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+def predictBitcoinPrice(date):
+  pred_data = {
+    'timestamp': [date]
+  }
+  predDf = pd.DataFrame(pred_data)
+  predArray = model.predict(predDf)
+  predPrice = predArray[0]
+  return predPrice
 
 app = Flask("Crypto predict API")
 
 @app.route('/bitcoin', methods=['GET'])
 def bitcoin():
   if(request.method == 'GET'):
+    dateInput = request.args.get('date')
+    parsedDate = datetimeParser.parse(dateInput)
+
     data = {
-      "price": 100,
-      "date": "2024-06-01",
+      "price": predictBitcoinPrice(parsedDate),
+      "date": parsedDate,
     }
     return jsonify(data)
 

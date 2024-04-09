abec4fa6cad99cfd7bb7b5795b357c37a3b39054
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-04-09 18:08:05 +0300

    Simplified trainFBP script
    Added updated dependencies to documentation

--- trainFBP.py
+++ trainFBP.py
@@ -1,30 +1,15 @@
 from alpaca.data.historical import CryptoHistoricalDataClient
 from alpaca.data.requests import CryptoBarsRequest
 from alpaca.data.timeframe import TimeFrame
-from datetime import datetime, timedelta
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
+from datetime import datetime
 import pandas as pd
-import dateutil.parser as datetimeParser
 import pickle
-from prophet.plot import plot_plotly, plot_components_plotly
-from prophet import Prophet 
-from statsmodels.tools.eval_measures import rmse
-
-#Set dataset and training parameters 
-datasetSize=365 #Set dataset size in days
-testSplitSize=50 #Set size of the test split for train and test sets
-forecastPeriod=8760 #Set the length of the forecast (in hours) year is 8760, 30 days is 720, 180 days is 4320
-selectedColumn="vwap" #Set which price data column to forecast by: open, high, low, close, vwap
+from prophet import Prophet
 
 client = CryptoHistoricalDataClient()
 
 current = datetime.now()
 current_date = current.strftime("%Y-%m-%d")
-startDate = (current - timedelta(days=datasetSize)).strftime("%Y-%m-%d")
-print(current_date)
-print(startDate)
 
 # Creating request object
 request_params = CryptoBarsRequest(
@@ -34,15 +19,19 @@ request_params = CryptoBarsRequest(
   end=current_date
 )
 
+print("Fetching training data...")
+
 data = client.get_crypto_bars(request_params)
+
+print("Fetched train data")
 
 df = data.df
 
 # Reset the index of the DataFrame
 df.reset_index(inplace=True)
 
-# Select the "timestamp" and "high" columns
-df = df[["timestamp", selectedColumn]]
+# Select only the "timestamp" and "close" columns
+df = df[["timestamp", "close"]]
 
 # Remove timezone information from the timestamps in the "ds" column
 df['timestamp'] = df['timestamp'].dt.tz_localize(None)
@@ -53,18 +42,13 @@ df.columns = ['ds','y']
 #convert timestamp to correct form
 df['ds'] = pd.to_datetime(df['ds'])
 
-# Split the dataframe into training and testing sets
-train = df.iloc[:len(df)-testSplitSize]
-test = df.iloc[len(df)-testSplitSize:]
+print("Training model...")
 
-m = Prophet(interval_width=0.8) #default 80& confidence
-m.fit(train)
-future = m.make_future_dataframe( periods=forecastPeriod, freq='H') #MS for monthly, H for hourly
-forecast = m.predict(future)
+model = Prophet(interval_width=0.8) #default 80& confidence
+model.fit(df)
 
-predictions = forecast.iloc[-testSplitSize:]['yhat']
+print("Model trained")
 
 #save model
-with open('modelFBPa.pkl', 'wb') as file:
-    pickle.dump(m, file)
-
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
